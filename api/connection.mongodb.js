360d455dbf1e2c4ef99f58ebcaeef5cd18e45d1d
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-25 19:48:23 +0330

    added a connection to mongodb

---
+++ api/connection.mongodb.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const colors = require('colors/safe');
+mongoose.connect('mongodb://admin:admin@ds235180.mlab.com:35180/url-shortner-db-ver2');
+
+mongoose.connection.once('open',()=>{
+    console.log(colors.green.bold('We Are Connected To MongoDB DBMS..!'));
+}).on('error',()=>{
+    console.warn('something went wrong on db!');
+});
