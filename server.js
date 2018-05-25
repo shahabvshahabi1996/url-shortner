35d815d401c361ef165efd7d13431d027bfd503c
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-05-25 19:47:13 +0330

    fixing bugses

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes');
 const path = require('path');
+const colors = require('colors');
+const cors = require('cors');
 
 const app = express();
 
-routes(app);
+require('./api/connection.mongodb');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+routes(app);
 
 let port = 8081;
 app.listen(port , () => {
-    console.log(`you are listening to the magic port : ${port}`)
+    console.log(`${colors.underline("you are listening to the magic port")} : ${colors.green.bold(port)}`)
 });
 
 
